c9c18fc97e6d5c3efdf95c33f4264c66cdc56578
Enronld <51634470+Enronld@users.noreply.github.com>
2021-05-26 21:42:57 +0800

    Delete 代码/ES管理工具/enron-es-api/src/src directory

--- 代码/ES管理工具/enron-es-api/src/src/main/java/com/enron/utils/ExceptionType.java
+++
@@ -1,9 +0,0 @@
-package com.enron.utils;
-
-public class ExceptionType {
-
-    //获取异常的类型
-    public String getExceptionType(Exception e){
-        return e.getMessage().substring(e.getMessage().indexOf("[")+1,e.getMessage().indexOf(",")).substring(5);
-    }
-}
